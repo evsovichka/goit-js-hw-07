b3c4d8fdcee4a4bd7a76aae617a0a9ad2d0d57e9
Yevsovych Kateryna <159715045+evsovichka@users.noreply.github.com>
2024-06-19 17:43:16 +0300

    task-4 + css task-1

--- js/task-4.js
+++ js/task-4.js
@@ -1,1 +1,21 @@
+const formForLogin = document.querySelector(".login-form");
 
+formForLogin.addEventListener("submit", handleSubmit);
+
+function handleSubmit(event) {
+  event.preventDefault();
+  const form = event.target;
+  const email = form.elements.email.value.trim();
+  const password = form.elements.password.value.trim();
+
+  if (email === "" || password === "") {
+    alert("All form fields must be filled in");
+    return;
+  }
+  const registr = {
+    Email: email,
+    Password: password,
+  };
+  console.log(registr);
+  formForLogin.reset();
+}
