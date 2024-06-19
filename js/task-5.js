a1e1b7d53cd428259a0849e05de81ae0d769665a
Yevsovych Kateryna <159715045+evsovichka@users.noreply.github.com>
2024-06-19 19:18:01 +0300

    task-5

--- js/task-5.js
+++ js/task-5.js
@@ -3,3 +3,14 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+const widget = document.querySelector(".widget");
+const button = document.querySelector(".change-color");
+const colorName = document.querySelector(".color");
+
+button.addEventListener("click", handleClick);
+
+function handleClick() {
+  const newColor = getRandomHexColor();
+  document.body.style.backgroundColor = newColor;
+  colorName.textContent = newColor;
+}
